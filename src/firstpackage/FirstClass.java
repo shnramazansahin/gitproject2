7131b1f19946c51574662767c19056b49a233c55
shnramazansahin <73439137+shnramazansahin@users.noreply.github.com>
2020-11-08 10:22:00 -0600

    first commit from remove master branch

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -8,6 +8,7 @@ public class FirstClass {
 		System.out.println("first commit from the local master branch");
 		System.out.println("secon commit from local master");
 		System.out.println("third commit from local master");
+		System.out.println("fourth commit from remote master");
 
 	}
 
