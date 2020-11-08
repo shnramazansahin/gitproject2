71eb0a3d7ecc9aaceafd777ed89653de9a256fb9
shnramazansahin <73439137+shnramazansahin@users.noreply.github.com>
2020-11-08 10:52:49 -0600

    update 2

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -9,6 +9,7 @@ public class FirstClass {
 		System.out.println("secon commit from local master");
 		System.out.println("third commit from local master");
 		System.out.println("first commit from remote master");
+		System.out.println("second commit from remote master");
 
 	}
 
